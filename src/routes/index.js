66a7749908661a887f7fe0efa9030a896f92cf81
Vishwa <patil.vishwa@gmail.com>
2018-03-04 13:14:06 +0530

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {Route, IndexRoute} from 'react-router'
+import Template from '../containers/Template'
+import TicTacToe from '../containers/TicTacToe'
+import Profile from '../containers/Profile'
+
+const createRoutes = () => {
+  return (
+    <Route
+      path='/'
+      component={Template}
+    >
+      <IndexRoute
+        component={TicTacToe}
+      />
+      <Route
+        path={'/profile'}
+        component={Profile}
+      />
+    </Route>
+  )
+}
+
+const Routes = createRoutes()
+
+export default Routes
